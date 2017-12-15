a5f04a849808903575a8260f5f44c156d8466e97
Daniel Kwok Zheng Xian <danielzhengxian21@gmail.com>
2017-12-15 15:28:38 +0800

    Incomplete RR

--- G52OSC-CW1/src/Process.java
+++ G52OSC-CW1/src/Process.java
@@ -1,31 +1,60 @@
+import java.util.*;
 
-public class Process {
-	public int AT;	//arrival time
-	public int BT;	//burst time
-	public String name;
-	static int i=1;
+public class Process implements Comparable<Process> {
+	public int AT;		//arrival time
+	public int BT;		//starting burst time
+	public int remBT;	//remaining burst time
+	public int tat=0;	//turn around time
+	public int wt=0;	//waiting time
+	public String name;	//name of process
+	private static int i=1;		//index of processes
+	public boolean completed = false;
+	Vector<Integer> workingct; //keeps track of all working currentTime
 	
 	public Process(int AT, int BT) {
+		
 		this.AT = AT;
 		this.BT = BT;
-		System.out.println("Creation of new node"+i);
-		i++;
-	}
-	
-	public Process(int num, int AT, int BT) {
-		name = "P"+num;
-		this.AT = AT;
-		this.BT = BT;
-		System.out.println("Creation of new node"+name);
+		remBT = BT;
+		name = "P"+i;
+		
+		workingct = new Vector<>();
+		workingct.add(AT);
+		
 		i++;
 	}
 	
 	public void ProcessInfo() {
 		System.out.print(name+":");
-		System.out.print(AT);
-		System.out.print(","+BT+"\n");
+		System.out.println("AT:"+AT+"|BT:"+BT+"|rem"+"BT:"+remBT);
+		System.out.println("completed: "+completed);
+		System.out.println("Workingct: "+workingct.toString());
 	}
 	
+	public void calculateTat() {
+		tat = workingct.lastElement() - workingct.firstElement();
+		System.out.println("tat for "+name+" is: "+tat);
+	}
+	
+	public void calculateWt() {
+		Vector<Integer> copyworkingct = workingct;
+		int n = copyworkingct.size();
+		
+		System.out.println(name);
+		
+		if(n>1) {
+			for(int t=0; t<n-1; t+=2) {
+				int startingTime = copyworkingct.get(t);
+				//System.out.println("startingtime is "+startingTime);
+				int endingTime = copyworkingct.get(t+1);
+				//System.out.println("endingTime is "+endingTime);
+				int tempwt = endingTime - startingTime;
+				//System.out.println("tempwt is "+tempwt);
+				wt += tempwt;
+			}
+			System.out.println(name+" wt is "+wt);
+		}
+	}
 
 	public boolean happenedWithin(int currentTime) {
 		if(AT<=currentTime) {
@@ -34,26 +63,53 @@ public class Process {
 		return false;
 	}
 	
-	public void processing(int tq) {
-		BT -= tq;
-		if(BT<0) 
-			BT=0;
+	//add process' starting time to workingct vector 
+	public void recordStartTime(int wt) {
+		workingct.add((Integer)wt);
+		System.out.println("Current working time is: "+wt);
+	}
+
+	//add process' finishing time to workingct vector 
+	public void recordFinishTime(int wt) {
+		System.out.println(name+"finished processing");
+		workingct.add((Integer)wt);
 	}
 	
-	public boolean finishProcessing() {
-		if(BT==0) {
+	public void processing(int wt) {
+		System.out.println("Processing "+name+" ...");
+		
+		remBT -= RR.TQ;
+		
+		//process finished in time less than TQ for particular cycle
+		if(remBT<0)
+			remBT=0;
+		ProcessInfo();
+	}
+	
+	public boolean completelyFinishProcessing() {
+		if(completed) {
 			return true;
-		}
+		}else if(remBT==0) {
+			System.out.println(name+" completely finished processing");
+			calculateTat();
+			calculateWt();
+			completed = true;
+			return true;
+		}		
 		return false;
+	}
+	
+	public int compareTo(Process p) {
+		return int.compare(p.AT, this.AT);
 	}
 }
 
 
-/*
-P1:0,4
-P2:1,5
-P3:2,6
-P4:4,1
-P5:6,3
-P6:7,2
-*/
+
+
+
+
+
+
+
+
--- G52OSC-CW1/src/RR.java
+++ G52OSC-CW1/src/RR.java
@@ -1,90 +1,110 @@
 import java.io.*;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.lang.management.ManagementFactory;
+import java.util.*;
+import com.sun.management.OperatingSystemMXBean;
 
 public class RR {
 	static Queue<Process> processesQueue = new LinkedList<>();
 	static LinkedList<Process> processesSlot = new LinkedList<>();
 	static Process[] processesArray;
+	static final int TQ = 3;	//time quantum
+	static int n;	//number of processes
+	
 	
 	public static void main(String[] args) throws Exception{
-		
-		int n=6;				//number of processes
-		int timeQuantum = 2;	//time quantum
-		
 		InputStreamReader isr = new InputStreamReader(System.in); // bytes to char
 		BufferedReader br = new BufferedReader(isr); 
 		
-		/*
 		System.out.print("Enter number of processes: ");
 		n = Integer.parseInt(br.readLine()); // char to int
-		*/
 		
 		allProcesses(n);
+		printPA();
 		
-		/*----------round robin algorithm----------*/
-		int currentTime = 0;
-		
-		
-		Process outStandingProcess = null;
-		int i=1;
-		//loop through all processes till all processes's BT == 0
-		
-		int next=0;	
-		while(notdone()) {
-			for(int l=0; l<n; l++) {
-				System.out.println("\n\nRound robin starting "+i);
-				System.out.println("Current time is "+currentTime);
+		long cputimeBefore = System.currentTimeMillis();
+		roundRobin(processesArray);
+		long cputimeAfter = System.currentTimeMillis();
+		long cpuTimeDifference = cputimeAfter - cputimeBefore;
 
+		System.out.println("CPU Time: " + cpuTimeDifference);
+		
+		printResult();
+		printAvetat();
+		printAvewt();
+		
+		printCPUInfo();
+	}
+	
+	public static void roundRobin(Process[] pa) {
+		int currentTime = 0;
+		int nextTime = currentTime;
+		Process workingProcess;
+		Process outStandingProcess = null;
+		int next=0;	
+		int n = pa.length;
+	
+		//while not all processes are finished
+		while(notdone()) {		
+			//for(int l=0; l<n; l++) {
+				printPA();
+				//printPS();
 				
-					System.out.println("Next is: "+next);
-					//add processes to queue
-					for(int j=next; j<n; j++) {
-						if(processesArray[j].happenedWithin(currentTime)) {
-							processesQueue.add(processesArray[j]);
-							next++;
-							System.out.println("Process added to queue is "+processesArray[j].name);
-						}
-					}
-				
-				
-				
-				System.out.println("Queue finish populated");
+				System.out.println("\n\nRR. Current time is "+currentTime);
 				printPQ();
 				
-				if(outStandingProcess!=null) {
-					System.out.println("Outstanding process from prev round was "+outStandingProcess.name);
-					processesQueue.add(outStandingProcess);	
-					printPQ();
-				}
-				
-				//add head of queue to working processes
-				Process workingProcess = processesQueue.poll();
-				processesSlot.add(workingProcess);
-				
-				//updating BT of current process being worked on
-				if(!workingProcess.finishProcessing()) {
-					workingProcess.processing(timeQuantum);	
-					
-					//current process being worked on becomes
-					//outstanding process is BT != 0
-					if(!workingProcess.finishProcessing()) {
-						outStandingProcess = workingProcess;
-						System.out.println("Outstanding process for this round is "+outStandingProcess.name);
-					}else {
-						outStandingProcess = null;
+				System.out.println("Adding to process queue...");
+				//add processes to queue
+				for(int j=next; j<n; j++) {
+					System.out.println("Current j is "+j);
+					processesArray[j].ProcessInfo();
+					if(processesArray[j].happenedWithin(currentTime)) {
+						processesQueue.add(processesArray[j]);
+						System.out.println(processesArray[j].name+" is added to queue");
+						next++;
+						System.out.println("New next is "+next);
 					}
 				}
+				if(outStandingProcess!=null) {
+					processesQueue.add(outStandingProcess);
+				}
 				
-			
-				System.out.println("End of round"+i);
-				printPS();
-				i++;
-				currentTime += timeQuantum;
-			}
-		}		
+				printPQ();
+				
+				if(!processesQueue.isEmpty()){
+					//add head of queue to working processes
+					workingProcess = processesQueue.poll();
+					processesSlot.add(workingProcess);
+					
+					//set next working time
+					if(workingProcess.remBT>=TQ) {
+						nextTime +=TQ;
+					}else {
+						nextTime += workingProcess.remBT;
+					}	
+					
+					//updating BT of current process being worked on
+					if(!workingProcess.completelyFinishProcessing()) {
+						workingProcess.recordStartTime(currentTime);
+						workingProcess.processing(currentTime);	
+						
+						//current process being worked on becomes
+						//outstanding process if BT != 0
+						if(!workingProcess.completelyFinishProcessing()) {
+							workingProcess.recordFinishTime(nextTime);
+							outStandingProcess = workingProcess;
+						}else {
+							outStandingProcess = null;
+						}
+					}
+				}else {
+					nextTime +=TQ;
+				}
+				currentTime = nextTime;	
+			//}
+			System.out.println("whilewhilewhilewhilewhilewhilewhilewhile");
+		}
 	}
 	
 	public static void allProcesses(int n) throws Exception{
@@ -95,13 +115,14 @@ public class RR {
 		System.out.println("Enter information for each processes");
 		processesArray = new Process[n];
 		
+		/*
 		//test case
-		Process p1 = new Process(1, 0, 4);
-		Process p2 = new Process(2, 1, 5);
-		Process p3 = new Process(3, 2, 6);
-		Process p4 = new Process(4, 4, 1);
-		Process p5 = new Process(5, 6, 3);
-		Process p6 = new Process(6, 7, 2);
+		Process p1 = new Process(0, 4);
+		Process p2 = new Process(1, 5);
+		Process p3 = new Process(2, 6);
+		Process p4 = new Process(4, 1);
+		Process p5 = new Process(6, 3);
+		Process p6 = new Process(7, 2);
 		processesArray[0] = p1;
 		processesArray[1] = p2;
 		processesArray[2] = p3;
@@ -109,8 +130,8 @@ public class RR {
 		processesArray[4] = p5;
 		processesArray[5] = p6;
 		printPA();
-		
-		/*for(int i=0; i<n; i++) {
+		*/
+		for(int i=0; i<n; i++) {
 			int AT;	//arrival time
 			int BT;	//burst time
 			
@@ -120,10 +141,11 @@ public class RR {
 			AT = Integer.parseInt(br.readLine()); // char to int
 			System.out.println(" Burst time: ");
 			BT = Integer.parseInt(br.readLine()); // char to int
-
 			Process newProcess = new Process(AT, BT);
+			processesArray[i] = newProcess;
 		}
-		*/
+		
+		Arrays.sort(processesArray);
 	}
 	
 	public static void printPA() {
@@ -135,8 +157,12 @@ public class RR {
 	
 	public static void printPQ() {
 		System.out.println("Printing processes queue");
-		for(Process p: processesQueue) {
-			p.ProcessInfo();
+		if(processesQueue.isEmpty()) {
+			System.out.println("Queue is empty");
+		}else {
+			for(Process p: processesQueue) {
+				p.ProcessInfo();
+			}			
 		}
 	}
 	
@@ -149,23 +175,45 @@ public class RR {
 	
 	public static boolean notdone() {
 		for(Process p:processesArray) {
-			if(p.BT!=0) {
-				return true;
+			if(!p.completed) {
+				return true;	//if even one isn't done, return true
 			}
 		}
-		return false;
+		return false;			//else return false
+	}
+	
+	public static void printResult() {
+		System.out.print(" ____________________________________________________________________");
+		System.out.println("\n|Process\t|BURST-TIME\t|WAITING-TIME\t|TURN AROUND-TIME   |");
+		for(Process p:processesArray)
+		{
+			System.out.println("--------------------------------------------------------------------");
+			System.out.println("|" + p.name + "\t\t|" + p.BT + "\t\t|" + p.wt + "\t\t|" + p.tat + "\t\t    |");
+		}
+		System.out.println(" ____________________________________________________________________\n");
+	}
+	
+	public static void printAvetat() {
+		float avetat = 0;
+		for(Process p:processesArray) {
+			avetat+=p.tat;
+		}
+		avetat = avetat/n;
+		System.out.println("Average turn around time: "+avetat);
+	}
+	
+	public static void printAvewt() {
+		float avewt = 0;
+		for(Process p:processesArray) {
+			avewt+=p.wt;
+		}
+		avewt = avewt/n;
+		System.out.println("Average waiting time: "+avewt);
+	}
+	
+	public static void printCPUInfo() {
+		OperatingSystemMXBean bean = (OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
+		System.out.println(bean.getProcessCpuLoad());
+		System.out.println("\n" + bean.getProcessCpuTime()+"nanoseconds");
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
